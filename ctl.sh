86dcdb75a8e6a2ba7e83c5bd81c564ef815d22fc
vbem <i@lilei.tech>
2020-09-26 20:49:07 +0800

    增加nginx代理配置示例

--- ctl.sh
+++ ctl.sh
@@ -14,31 +14,36 @@ DIR_CACHE=$DIR_HOME/cache
 FILE_THIS=$(basename ${BASH_SOURCE[0]})
 PATH_CONF=$DIR_HOME/conf/client.conf
 
+source $PATH_CONF || { echo "请初始化配置文件 $PATH_CONF"; exit 255; }
+
+
 FLAGS_COMMON="-v --stats=30s --stats-one-line"
 FLAGS_VFS="--dir-cache-time=1m --cache-dir=$DIR_CACHE --vfs-cache-mode=writes --vfs-cache-max-age=10m --vfs-cache-max-size=1g"
-FLAGS_DSFTP=':sftp: --sftp-host=${DSFTP_ENDPOINT} --sftp-user=${DSFTP_USER} --sftp-pass=$(rclone obscure "${DSFTP_PASS}")'
+FLAGS_DSFTP=":sftp: --sftp-host=${DSFTP_ENDPOINT} --sftp-user=${DSFTP_USER} --sftp-pass=$(rclone obscure ${DSFTP_PASS})"
 
 IS_LOGGING=1
 
-HELP="
-$FILE_THIS - DSFTP客户端控制器 https://github.com/opgcn/dsftp-client
+HELP="$FILE_THIS - DSFTP客户端控制器 https://github.com/opgcn/dsftp-client
+
+当前配置文件$PATH_CONF:
+$(grep -E -v '^[[:space:]]*$|^[[:space:]]*#' conf/client.conf | sed 's/^/    /g')
 
 用法:
-    open    使用lftp工具交互式访问DSFTP
-    rclone  对操作系统重新安装rclone工具
+    lftp    使用lftp工具交互式访问DSFTP
+    sftp    使用sftp工具交互式访问DSFTP
     tree    使用rclone工具显示DSFTP的目录树
     lsl     使用rclone工具列取DSFTP中所有文件的大小/时间/路径
     size    使用rclone工具统计DSFTP中所有文件数量和大小
     explore 使用rclone工具以交互式方式访问DSFTP
-    http    使用rclone工具代理成本地HTTP服务
-    sftp    使用rclone工具代理成本地SFTP服务
+    http    使用rclone工具代理DSFTP为本地HTTP协议服务
+    ftp     使用rclone工具代理DSFTP为本地FTP协议服务
+    nginx   显示nginx本地反代DSFTP四层SFTP协议的配置示例
     mount   使用sshfs工具将DSFTP挂载到本地 $DIR_MNT/
     umount  使用fusermount工具将本地挂载点取消
     help    显示此帮助
 "
 
-HELP_NCDU="
-交互式浏览器中快捷键提示：
+HELP_NCDU="交互式浏览器中快捷键提示：
 
  ↑      向上移动光标
  ↓      向下移动光标
@@ -57,6 +62,47 @@ HELP_NCDU="
  q/ESC  退出交互式GUI浏览器
 
 按任意键继续....
+"
+
+HELP_RCLONE="
+rm -rf $DIR_TMP \\
+&& mkdir -p $DIR_TMP \\
+&& cd $DIR_TMP \\
+&& wget https://downloads.rclone.org/rclone-current-linux-amd64.zip \\
+&& unzip rclone-current-linux-amd64.zip \\
+&& cd rclone-*-linux-amd64 \\
+&& sudo cp -f rclone /usr/bin/ \\
+&& sudo chown root:root /usr/bin/rclone \\
+&& sudo chmod 755 /usr/bin/rclone \\
+&& sudo mkdir -p /usr/local/share/man/man1 \\
+&& sudo cp rclone.1 /usr/local/share/man/man1/ \\
+&& sudo mandb \\
+&& cd $DIR_HOME \\
+&& rm -rf $DIR_TMP \\
+&& rclone version
+"
+
+HELP_NGINX="
+# http://nginx.org/en/docs/ngx_core_module.html
+user                nginx;
+worker_processes    auto;
+error_log           /var/log/nginx/error.log warn;
+pid                 /var/run/nginx.pid;
+include             /usr/share/nginx/modules/*.conf;
+
+events {
+    use                 epoll;
+    multi_accept        on;
+    worker_connections  1024;
+} # events
+
+# http://nginx.org/en/docs/stream/ngx_stream_core_module.html
+stream {
+    server {
+        listen 1;  # 本地代理SFTP协议的端口
+        proxy_pass $DSFTP_ENDPOINT:22;
+    }
+} # stream
 "
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -91,7 +137,7 @@ function checkConf
 function checkRclone
 {
     [ "$(which rclone 2> /dev/null)" ] && return 0
-    echoDebug FATAL "rclone工具未正确安装!" && return 254
+    echoDebug FATAL "rclone工具未正确安装! 请参考 https://rclone.org/install/ , 或使用如下命令安装:$HELP_RCLONE" && return 254
 }
 
 function checkLftp
@@ -113,6 +159,17 @@ function prepareMntDir
     return 0
 }
 
+function checkSshpass
+{
+    [ "$(which sshpass 2> /dev/null)" ] && return 0
+    echoDebug FATAL "sshpass工具未正确安装! 请使用'sudo yum install -y sshpass'等方式安装" && return 247
+}
+
+function getDsftpRcloneAddrOpts
+{
+    echo :sftp: --sftp-host=${DSFTP_ENDPOINT} --sftp-user=${DSFTP_USER} --sftp-pass=$(rclone obscure ${DSFTP_PASS})
+}
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # process functions
 
@@ -121,61 +178,40 @@ function main
     declare sOpt="$1"
     if [ "$sOpt" == "help" ] || [ "$sOpt" == '' ]; then
         echo "$HELP"
-    elif [ "$sOpt" == "open" ]; then
-        echoDebug INFO "开始使用lftp工具交互式访问DSFTP...."
+    elif [ "$sOpt" == "lftp" ]; then
         checkConf && checkLftp \
         && runCmd lftp -u ${DSFTP_USER},${DSFTP_PASS} -e 'help' sftp://${DSFTP_ENDPOINT}
-    elif [ "$sOpt" == "rclone" ]; then
-        echoDebug INFO "开始重新安装rclone工具...."
-        runCmd rm -rf $DIR_TMP \
-        && runCmd mkdir -p $DIR_TMP \
-        && runCmd cd $DIR_TMP \
-        && runCmd wget https://downloads.rclone.org/rclone-current-linux-amd64.zip \
-        && runCmd unzip rclone-current-linux-amd64.zip \
-        && runCmd cd rclone-*-linux-amd64 \
-        && runCmd sudo cp -f rclone /usr/bin/ \
-        && runCmd sudo chown root:root /usr/bin/rclone \
-        && runCmd sudo chmod 755 /usr/bin/rclone \
-        && runCmd sudo mkdir -p /usr/local/share/man/man1 \
-        && runCmd sudo cp rclone.1 /usr/local/share/man/man1/ \
-        && runCmd sudo mandb \
-        && runCmd cd $DIR_HOME \
-        && runCmd rm -rf $DIR_TMP \
-        && runCmd rclone version
+    elif [ "$sOpt" == "sftp" ]; then
+        checkConf && checkSshpass \
+        && runCmd sshpass -p ${DSFTP_PASS} sftp -C ${DSFTP_USER}@${DSFTP_ENDPOINT}
     elif [ "$sOpt" == "tree" ]; then
-        echoDebug INFO "开始使用rclone工具列取DSFTP的目录树...."
         checkConf && checkRclone \
-        && runCmd rclone tree -aC --dirsfirst $(eval "echo $FLAGS_DSFTP")
+        && runCmd rclone tree -aC --dirsfirst $FLAGS_DSFTP
     elif [ "$sOpt" == "lsl" ]; then
-        echoDebug INFO "开始使用rclone工具列取DSFTP中所有文件的大小/时间/路径...."
         checkConf && checkRclone \
-        && runCmd rclone lsl $(eval "echo $FLAGS_DSFTP")
+        && runCmd rclone lsl $FLAGS_DSFTP
     elif [ "$sOpt" == "explore" ]; then
-        echoDebug INFO "开始使用rclone工具以交互式方式访问DSFTP...."
         checkConf && checkRclone \
         && read -n 1 -s -r -p "$HELP_NCDU" \
-        && runCmd rclone ncdu $(eval "echo $FLAGS_DSFTP")
+        && runCmd rclone ncdu $FLAGS_DSFTP
     elif [ "$sOpt" == "size" ]; then
-        echoDebug INFO "开始使用rclone工具统计DSFTP中所有文件数量和大小...."
         checkConf && checkRclone \
-        && runCmd rclone size $(eval "echo $FLAGS_DSFTP")
+        && runCmd rclone size $FLAGS_DSFTP
     elif [ "$sOpt" == "http" ]; then
-        echoDebug INFO "开始使用rclone工具代理成本地HTTP服务...."
         checkConf && checkRclone \
-        && runCmd rclone serve http $(eval "echo $FLAGS_DSFTP") $FLAGS_COMMON $FLAGS_VFS $DSFTP_PROXY_HTTP_OPTS
-    elif [ "$sOpt" == "sftp" ]; then
-        echoDebug INFO "开始使用rclone工具代理成本地SFTP服务...."
+        && runCmd rclone serve http $FLAGS_COMMON $FLAGS_VFS $DSFTP_PROXY_HTTP_OPTS $FLAGS_DSFTP
+    elif [ "$sOpt" == "ftp" ]; then
         checkConf && checkRclone \
-        && runCmd rclone serve sftp $(eval "echo $FLAGS_DSFTP") $FLAGS_COMMON $FLAGS_VFS $DSFTP_PROXY_SFTP_OPTS
+        && runCmd rclone serve ftp $FLAGS_COMMON $FLAGS_VFS $DSFTP_PROXY_FTP_OPTS $FLAGS_DSFTP
+    elif [ "$sOpt" == "nginx" ]; then
+        checkConf && echo "$HELP_NGINX"
     elif [ "$sOpt" == "mount" ]; then
-        echoDebug INFO "开始使用sshfs工具将DSFTP挂载到本地目录 $DIR_MNT/ ...."
         sCmd1="echo ${DSFTP_PASS}"
         sCmd2="sshfs ${DSFTP_USER}@${DSFTP_ENDPOINT}:/ $DIR_MNT -C -o password_stdin,StrictHostKeyChecking=no,PreferredAuthentications=password,reconnect,ServerAliveInterval=15,ServerAliveCountMax=3"
         checkConf && checkSshfs && prepareMntDir \
-        && echoDebug INFO "开始执行命令: $sCmd1 | $sCmd2" && $sCmd1 | $sCmd2 \
+        && echoDebug DEBUG "开始执行命令: $sCmd1 | $sCmd2" && $sCmd1 | $sCmd2 \
         && echoDebug INFO "sshfs调用结束，请查看目录 $DIR_MNT/ !"
     elif [ "$sOpt" == "umount" ]; then
-        echoDebug INFO "开始使用fusermount工具取消挂本地载点 $DIR_MNT/ ...."
         checkConf && checkSshfs \
         && runCmd fusermount -u $DIR_MNT
     else
@@ -187,10 +223,4 @@ function main
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # main process
-
-source $PATH_CONF
-if [ 0 -ne $? ]; then
-    echoDebug FATAL "无法加载配置文件 $PATH_CONF"
-    exit 255
-fi
 main $@
